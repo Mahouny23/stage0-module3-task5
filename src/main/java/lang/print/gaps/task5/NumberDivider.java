473eb70e9d5d51be0b9609b9faaf5e0de988f89e
Madi <madiasunik@gmail.com>
2022-08-10 23:43:32 +0600

    Module 3 Task #5 - commit #1

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        toBeDivided = 19;
+        float answer = toBeDivided / 5f;
+        System.out.println(answer);
     }
 }
--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -1,6 +1,11 @@
 package lang.print.gaps.task5;
 
+import java.util.Scanner;
+
 public class TimeConvertor {
     public void convert(float minutes) {
-    }
+        minutes = 11.5F;
+        float seconds = minutes * 60;
+        System.out.println(seconds);
+      }
 }
